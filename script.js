cf9c78b22eda40d85e9f24cc3d2bb5e1772ac273
knetto <106658082+knetto@users.noreply.github.com>
2026-01-05 23:42:43 +0100

    Add files via upload
    false added 1e minigame

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ let currentLine = 0;
 let isModalOpen = false;    
 
 // Locks (Zet ze op false om de speler te dwingen de game te spelen)
-let level1Unlocked = true; 
+let level1Unlocked = false; 
 let level2Unlocked = false; 
 
 
